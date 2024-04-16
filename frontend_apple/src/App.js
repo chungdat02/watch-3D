b08d74dafe30471adc79869f50008df18152ccae
NooNekk <93216369+NooNekk@users.noreply.github.com>
2024-04-16 08:31:36 +0700

    CaoTriTinh upload front-end/src

---
+++ frontend_apple/src/App.js
@@ -0,0 +1,84 @@
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DefaultComponent from "./components/DefaultComponent/DefaultComponent.jsx";
+import routes from "./routes";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+// import dotenv from "dotenv";
+// dotenv.config();
+import CheckoutPage from "./CheckoutPage"; 
+// function App() {
+//   // useEffect(() => {
+//   //   fetchApi();
+//   // }, []);
+
+//   // const fetchApi = async () => {
+//   //   // console.log("url", process.env.REACT_APP_API_URL);
+//   //   const res = await axios.get(
+//   //     `${process.env.REACT_APP_API_URL}/product/get-all`
+//   //   );
+//   //   return res.data;
+//   // };
+//   // const query = useQuery({ queryKey: ["todos"], queryFn: fetchApi });
+//   // console.log("query", query);
+//   return (
+//     <div>
+//       <Router>
+//         <Routes>
+//           {routes.map((route, index) => {
+//             const Page = route.page;
+//             const Layout = route.isShowHeader ? DefaultComponent : Fragment;
+//             return (
+//               <Route
+//                 key={index}
+//                 path={route.path}
+//                 element={
+//                   <Layout>
+//                     <Page />
+//                   </Layout>
+//                 }
+//               />
+//             );
+//           })}
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+// export default App;
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          {routes.map((route, index) => {
+            const Page = route.page;
+            const Layout = route.isShowHeader ? DefaultComponent : Fragment;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+          {/* Thêm route cho CheckoutPage */}
+          <Route
+            path="/checkout"
+            element={
+              <DefaultComponent>
+                <CheckoutPage />
+              </DefaultComponent>
+            }
+          />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend_apple/src/components/CardComponent/CardComponent.jsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import {
+  StyleNameProduct,
+  WrapperCardStyle,
+  WrapperImagesStyle,
+  WrapperPriceText,
+} from "./style";
+import logo from "../../assets/images/kiemduyet.png";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart } from "../../redux/cartSlice";
+import * as message from "../../components/Message/Message";
+
+const CardComponent = ({ data }) => {
+  const dispatch = useDispatch();
+
+  const [listIdCart, setListIdCart] = useState([]);
+  const cart = useSelector((state) => state.cart.cart);
+  const handleAddToCart = () => {
+    dispatch(addToCart({ info: data, amount: 1 }));
+    message.success("Thêm giỏ hàng thành công");
+  };
+
+  useEffect(() => {
+    const listId = cart?.map((e) => e?.info?._id);
+    setListIdCart(listId);
+  }, [cart]);
+
+//   const redirectToProductPage = () => {
+//     window.location.href = 'http://localhost:5500/frontend_apple/clock/clock.html';
+//     console.log(window.location.href);
+// };
+
+  return (
+    <div>
+      <WrapperCardStyle
+        bodyStyle={{ padding: "10px" }}
+        hoverable
+        headStyle={{ width: "300px", height: "300px" }}
+        style={{
+          width: 200,
+        }}
+        cover={<img alt="example" src={data?.image} />}
+      >
+        <WrapperImagesStyle preview={false} src={logo} alt="#" />
+        <StyleNameProduct>{data?.name}</StyleNameProduct>
+        <WrapperPriceText
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            paddingTop: "3px",
+          }}
+        >
+          <span style={{ marginRight: "8px" }}>
+            {data?.price.toLocaleString("it-IT", {
+              style: "currency",
+              currency: "VND",
+            })}
+          </span>
+        </WrapperPriceText>
+        {listIdCart?.includes(data?._id) ? (
+          <button
+            style={{
+              width: "100%",
+              height: "35px",
+              background: "green",
+              borderRadius: "10px",
+              color: "white",
+              marginBottom: "5px", // Thêm khoảng cách giữa các nút
+            }}
+          >
+            Đã có trong giỏ hàng
+          </button>
+        ) : (
+          <button
+            style={{
+              width: "100%",
+              height: "35px",
+              background: "red",
+              borderRadius: "10px",
+              color: "white",
+              marginBottom: "5px", // Thêm khoảng cách giữa các nút
+            }}
+            onClick={handleAddToCart}
+          >
+            Thêm giỏ hàng
+          </button>
+        )}
+        <a
+          style={{
+            display: "block",
+            textAlign: "center",
+            width: "100%",
+            lineHeight: "35px",
+            background: "blue",
+            borderRadius: "10px",
+            color: "white",
+          }}
+         href={`${data?.view3d ? `http://localhost:5500/frontend_apple/clock/${data.view3d}` : "#!"}`}
+        >
+          Xem chi tiết
+        </a>
+      </WrapperCardStyle>
+    </div>
+  );
+};
+
+export default CardComponent;
