0a94fb32a53e91d4d0cf8441b7dd99da60fedd81
NooNekk <93216369+NooNekk@users.noreply.github.com>
2024-04-16 12:27:14 +0700

    check code auth

--- api_nodejs/src/middleware/authMiddleware.js
+++ api_nodejs/src/middleware/authMiddleware.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const authMiddleware = (req, res, next) => {
   const token = req.headers.token.split(" ")[1];
+  console.log(token);
   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
     if (err) {
       return res.status(404).json({
@@ -24,6 +25,7 @@ const authMiddleware = (req, res, next) => {
 const authUserMiddleware = (req, res, next) => {
   const token = req.headers.token.split(" ")[1];
   const userId = req.params.id;
+  console.log(data?.ACCESS_TOKEN);
   jwt.verify(token, process.env.ACCESS_TOKEN, function (err, user) {
     if (err) {
       return res.status(404).json({
